8f2bd1302d2213db92baec7fd68d9f959827ee7e
honglujun <909408954@qq.com>
2016-08-11 14:17:40 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i=0;i<10;i++){
+            System.out.println("helloworld!");
+            System.out.println("i="+i);
+        }
+    }
+}
